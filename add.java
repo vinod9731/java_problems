64d4ada9b7b007e3732fb8da8740863dd58543da
vinod <158268202+vinod9731@users.noreply.github.com>
2024-11-19 20:01:14 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int d=a+b+c;
+        System.out.println("Sum of a,b,c is "+d);
+}
+}
