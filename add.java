297cd39f147047af63dfa229c3a9c26b6db0d421
vinod <158268202+vinod9731@users.noreply.github.com>
2026-03-04 13:55:01 +0530

    Refactor SQL queries for unmatched payments and KYC

--- add.java
+++ add.java
@@ -62,3 +62,51 @@ ON ltrim(p.PAY_OPE_NUM,'0') =
    ltrim(CAST(pos.POS_OPERATION_NO AS VARCHAR),'0')
 AND p.PAY_ACCOUNT_KEY = pos.POS_ACCOUNT_KEY
 WHERE pos.POS_ID IS NULL;
+
+scre2
+
+SELECT 
+    p.PAY_ID,
+    p.PAY_INTRBKSTTLMAMT,
+    pos.AML_SCOPE,
+    pos.AML_EXCL_CODE
+FROM payment.default.payment_init p
+JOIN payment.default.posting_init pos
+ON ltrim(p.PAY_OPE_NUM,'0') =
+   ltrim(CAST(pos.POS_OPERATION_NO AS VARCHAR),'0')
+AND p.PAY_ACCOUNT_KEY = pos.POS_ACCOUNT_KEY
+
+WHERE p.PAY_INTRBKSTTLMAMT > 10000
+AND (
+      pos.AML_SCOPE IS NULL
+      OR pos.AML_EXCL_CODE IS NULL
+);
+
+SELECT COUNT(*) AS kyc_missing_transactions
+FROM payment.default.payment_init p
+JOIN payment.default.posting_init pos
+ON ltrim(p.PAY_OPE_NUM,'0') =
+   ltrim(CAST(pos.POS_OPERATION_NO AS VARCHAR),'0')
+AND p.PAY_ACCOUNT_KEY = pos.POS_ACCOUNT_KEY
+
+WHERE p.PAY_INTRBKSTTLMAMT > 10000
+AND (
+      pos.AML_SCOPE IS NULL
+      OR pos.AML_EXCL_CODE IS NULL
+);
+
+
+
+SELECT COUNT(*) AS kyc_missing_after
+FROM payment.default.payment_after p
+JOIN payment.default.posting_after pos
+ON ltrim(p.PAY_OPE_NUM,'0') =
+   ltrim(CAST(pos.POS_OPERATION_NO AS VARCHAR),'0')
+AND p.PAY_ACCOUNT_KEY = pos.POS_ACCOUNT_KEY
+
+WHERE p.PAY_INTRBKSTTLMAMT > 10000
+AND (
+      pos.AML_SCOPE IS NULL
+      OR pos.AML_EXCL_CODE IS NULL
+);
+
