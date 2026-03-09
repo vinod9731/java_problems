fa3f7522f2a1c59d3286f13aaf3a75cdbcdf638f
vinod <158268202+vinod9731@users.noreply.github.com>
2026-03-09 12:47:37 +0530

    Remove Kafka screening processor and add Neo4j query

--- add.java
+++ add.java
@@ -1,51 +1,8 @@
-const { createConsumer, producer } = require('../config/kafka');
+"neo4j.cypher.topic.kyb.risk.scored":
+"MATCH (c:KybCase {id:value.caseId}) MERGE (r:Risk {caseId:value.caseId}) SET r.score = value.score, r.factors = value.factors MERGE (c)-[:HAS_RISK]->(r)"
 
-async function startScreeningProcessor() {
 
-  const consumer = createConsumer('screening-group');
+  curl -X DELETE http://localhost:8083/connectors/neo4j-sink-kyb
 
-  await consumer.connect();
-  await producer.connect();
 
-  await consumer.subscribe({
-    topic: 'kyb.ubo.discovered',
-    fromBeginning: true
-  });
-
-  await consumer.run({
-    eachMessage: async ({ message }) => {
-
-      const payload = JSON.parse(message.value.toString());
-
-      const { caseId, ubos, industry, country } = payload;
-
-      console.log(`Screening case ${caseId}`);
-
-      const sanctionsList = ['John Doe'];
-
-      const screeningResults = ubos.map(ubo => ({
-        personId: ubo.personId,
-        name: ubo.name,
-        hit: sanctionsList.includes(ubo.name)
-      }));
-
-      await producer.send({
-        topic: 'kyb.screening.completed',
-        messages: [
-          {
-            value: JSON.stringify({
-              caseId,
-              industry,
-              country,
-              screeningResults
-            })
-          }
-        ]
-      });
-
-      console.log('Screening message emitted');
-    }
-  });
-}
-
-module.exports = { startScreeningProcessor };
+  
